35899ec0e6a02156fd768b34c26192e1abae55d9
Julia Pritychenko <juliasnoww@mail.ru>
2018-09-25 10:48:48 +0300

    equation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
-}
+    let array = equation.split(' ');
+    let a = parseInt(array[0]);
+    let b = parseInt(array[3] + array[4]);
+    let c = parseInt(array[7] + array[8]);
+    let discriminant = Math.sqrt(b * b - 4 * a * c);
+    let roots = [];
+    roots[0] = Math.round((-b - discriminant) / (2 * a));
+    roots[1] = Math.round((-b + discriminant) / (2 * a));
+    return roots.sort(((left, right) => left - right));
+};
+    
